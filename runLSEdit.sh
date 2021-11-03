0405df2d20c2f6aebbe4268e118164e0e0b7d282
Derevi <34952812+Derevi@users.noreply.github.com>
2021-11-02 20:33:31 -0400

    Update runLSEdit.sh

--- runLSEdit.sh
+++ runLSEdit.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Original line provided
 #java  -Xms256M -Xmx1024M -jar lseditor-7.3.13.jar c488_UnderstandFileDependency.con.ta
+
+# Edited line, the first argument is the .ta file you want to load
 java  -Xms256M -Xmx1024M -jar lseditor-7.3.13.jar $1
